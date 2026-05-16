4ee781372359586385f7d8b9c3db3c51fcbdda30
Rich Marafioti <richmarafioti@gmail.com>
2026-05-16 11:55:00 -0500

    feat: add info cards to landing page

--- src/app/_data/photos.js
+++ src/app/_data/photos.js
@@ -26,6 +26,53 @@ export const businessExterior = {
   },
 };
 
+export const infoCards = [
+  {
+    id: 1,
+    header: "Food & Supplies",
+    copy: "Lonem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna am, quis nostnud.",
+    icon: {
+      src: "https://res.cloudinary.com/dzpne110u/image/upload/v1778270981/pet_bodega/assets/bowl_m7gqgf.svg",
+      height: 116,
+      width: 196,
+      alt: "Dog bowl icon",
+    },
+  },
+  {
+    id: 2,
+    header: "Treats & Toys",
+    copy: "Lonem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna am, quis nostnud.",
+    icon: {
+      src: "https://res.cloudinary.com/dzpne110u/image/upload/v1778270980/pet_bodega/assets/treat_gyugzz.svg",
+      height: 209,
+      width: 164,
+      alt: "Fish bone icon",
+    },
+  },
+  {
+    id: 3,
+    header: "Grooming",
+    copy: "Lonem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna am, quis nostnud.",
+    icon: {
+      src: "https://res.cloudinary.com/dzpne110u/image/upload/v1778270980/pet_bodega/assets/shears_w5rwrw.svg",
+      height: 209,
+      width: 221,
+      alt: "Gromming scissors icon",
+    },
+  },
+  {
+    id: 4,
+    header: "Self-wash",
+    copy: "Lonem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna am, quis nostnud.",
+    icon: {
+      src: "https://res.cloudinary.com/dzpne110u/image/upload/v1778270981/pet_bodega/assets/bubbles_wknviy.svg",
+      height: 193,
+      width: 177,
+      alt: "Dog bath bubbles icon",
+    },
+  },
+];
+
 export const icons = {
   alt: "website icon and home page button",
   src_light:
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,34 @@
 "use client";
 
-import { heroPhoto, businessExterior } from "./_data/photos";
+import Image from "next/image";
+import { heroPhoto, businessExterior, infoCards } from "./_data/photos";
 import FadeInSection from "./_components/Fade_In_Section";
 import Responsive_Image_Layout from "./_components/Responsive_Image_Layout";
 import Contact_Form from "./_components/forms/Contact_Form";
 
 import styles from "./_styling/landing_page.module.css";
+
+function InfoCard() {
+  return (
+    <div className={styles.info_section}>
+      {infoCards.map(({ id, header, copy, icon }) => (
+        <div key={id} className={styles.info_card}>
+          <Image
+            src={icon.src}
+            height={icon.height}
+            width={icon.width}
+            alt={icon.alt}
+            className={styles.icon}
+          />
+          <div className={styles.copy}>
+            <p className={styles.title}>{header}</p>
+            <p>{copy}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
@@ -15,14 +38,23 @@ export default function Home() {
           <Responsive_Image_Layout photoData={heroPhoto} />
         </div>
         <div className={styles.copy_section}>
-          <p>Come for the baths,</p>
-          <p>Stay for the vibes.</p>
+          <h1>Come for the baths,</h1>
+          <h1>Stay for the vibes.</h1>
         </div>
         <div className={styles.image_container}>
           <Responsive_Image_Layout photoData={businessExterior} />
         </div>
-
-        <Contact_Form />
+        <div className={styles.copy_section} id={styles.copy_section_blk}>
+          <h2 className={styles.info_header}>We have the best shit...</h2>
+          <InfoCard />
+        </div>
+        <div className={styles.copy_section}>
+          <h3>@the.pet.bodgea</h3>
+        </div>
+        <div className={styles.copy_section} id={styles.copy_section_blk}>
+          <p>next section</p>
+        </div>
+        {/*<Contact_Form />*/}
       </FadeInSection>
     </main>
   );
